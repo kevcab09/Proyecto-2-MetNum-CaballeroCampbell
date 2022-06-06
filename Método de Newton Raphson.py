65d96252721d5fb5235261c630a2e6df0eb42699
kevcab09 <kevin.caballero3@utp.ac.pa>
2022-06-06 12:15:03 -0500

    Se modifico el metodo de newton raphson

--- Método de Newton Raphson.py
+++ Método de Newton Raphson.py
@@ -4,23 +4,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#Función que calcula el valor de la expresion
-def fx(x):
-    f = x**3 - 2*x -5
-    return f
+#Función para ingresar la ecuacion
+def fx(x, t1, t2, t3, c1, c2, c3):
+    return t1* x ** c1 + t2 * x **c2 + t3 * x ** c3
 
-#Función que calcula el valor de la derivada
-def Dfx(x):
-    deriv_f = 3*x**2 - 2 
-    if deriv_f == 0:
-        print("Cuidado: La derivada de la funcion es igual a: 0")
-        exit()
-    return deriv_f
+#Funcion para ingresar la derivada
+def dfx(x, et1, et2, et3, ec1, ec2, ec3):
+    return et1* x ** ec1 + et2 * x **ec2 + et3 * x ** ec3
 
 #Programa principal
+#Ingresar los valores
+print("Ingrese los terminos solicitados")
+print("Para el término independiente escriba 0 en el exponente")
+t1 = float (input ("Primer término de la funcion: "))
+t2 = float (input ("Segundo término de la funcion: "))
+t3 = float (input ("Tercer término de la funcion: "))
+c1 = float (input ("Primer exponente de la funcion: "))
+c2 = float (input ("Segundo exponente de la funcion: "))
+c3 = float (input ("Tercer exponente de la funcion: "))
 
+et1 = float (input ("Primer término de la funcion derivada: "))
+et2 = float (input ("Segundo término de la funcion derivada: "))
+et3 = float (input ("Tercer término de la funcion derivada: "))
+ec1 = float (input ("Primer exponente de la funcion derivada: "))
+ec2 = float (input ("Segundo exponente de la funcion derivada: "))
+ec3 = float (input ("Tercer exponente de la funcion derivada: "))
+
+#Grafica inicial
 x = np.linspace(-25,25,150)
-plt.plot(x, fx(x))
+plt.plot(x,fx(x, t1, t2, t3, c1, c2, c3))
 plt.axhline(0, color = "green")
 plt.axvline(0, color = "green")
 plt.ylim(-25, 25)
@@ -35,45 +47,28 @@ xi = float(input())
 #Error que queremos
 print("Ingrese el error tolerado:")
 err_q = float(input())
+#Iteraciones 
+print("Ingrese el limite de iteraciones:")
+limite = int(input())
 
-#Error relativo porcentual
-err_r = 100.0
+#Encabezado
+print("{:^5} {:^15} {:^15} {:^15} {:^15} {:^15} ".format("Iteracion", "x0", "xsolucion", "Error", "f(x0)", "f'(x0)"))
 
-#Primera iteración
-#Llama  a la función que calcula el valor de f(x)
-fxi = fx(xi)
-
-#Llama a la función que calcula el valor de la derivada de f(x)
-Dfxi = Dfx(xi)
-
-#Calcula el siguiente estimado de la solución
-xsol = xi - fxi / Dfxi
-
-#Operaciones
-#Contador de iteraciones
-niter = 1
-#Impresión en pantalla
-print("{:^5} {:^15} {:^15} {:^15} {:^15} {:^15} ".format("Iter", "xi", "f(xi)", "f'(xi)", "xsol", "err_r"))
-print("{:^4d} {:^15.4f} {:^15.4f} {:^15.4f} {:^15.6f} {:^15.4f}".format(niter, xi, fxi, Dfxi, xsol, err_r))
-#Ciclo while hasta obtener una solución con el error menor al error pedido
-while(err_r >= err_q):
-#Respalda el valor actual de la solución
-    xi = xsol
-#Llama  a la función que calcula el valor de f(x)
-    fxi = fx(xi)
-#Llama a la función que calcula el valor de la derivada de f(x)
-    Dfxi = Dfx(xi)
-#Calcula el siguiente estimado de la solución
-    xsol = xi - fxi / Dfxi
-
-#Número de iteraciones
-    niter = niter + 1
-#Calcular el error relativo porcentual
-    err_r = abs((xsol - xi) / xsol) * 100
-
-#Variables de las iteraciones
-    print("{:^4d} {:^15.4f} {:^15.4f} {:^15.4f} {:^15.6f} {:^15.4f}".format(niter, xi, fxi, Dfxi, xsol, err_r))
-
-print("\n\nFin del método de Newton Raphson con xsol =", round(xsol,4))
-
+for k in range(limite):
+    x1=xi-fx(xi, t1, t2, t3, c1, c2, c3)/dfx(xi, et1, et2, et3, ec1, ec2, ec3)
+    error=abs((x1-xi)/x1)
+    print("{:^4d} {:^20.6f} {:^15.6f} {:^15.6f} {:^15.6f} {:^15.6f}".format(k, xi, x1, error, fx(xi, t1, t2, t3, c1, c2, c3), dfx(xi, et1, et2, et3, ec1, ec2, ec3)))
+    if error<err_q:
+        break
+    xi=x1
+x = np.linspace(-20,20,100)
+plt.plot(x, fx(x, t1, t2, t3, c1, c2, c3))
+plt.plot(xi, fx(xi, t1, t2, t3, c1, c2, c3), 'or')
+plt.axhline(0, color="green")
+plt.axvline(0, color="green")
+plt.ylim(-25, 25)
+plt.grid()
+plt.show()
+  
+print("\n\nFin del método de Newton Raphson con xsol =", round(x1,4))
 print('\n Parcial N°2 \n Metodo Regula-Falsi y Newton Raphson \n Carlos "La pistola" Campbell y Kevin "El bombardero" Caballero')
